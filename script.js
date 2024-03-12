f94905bc3a83997fd94c10ed38c89d351fc44602
Jake-Silveira <jacob1silveira@gmail.com>
2024-03-12 16:25:32 -0500

    duplicated admin dashboard for template

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title + ',';
+  this.author = ' by ' + author + ', ';
+  this.pages = pages + ' pages, ';
+  this.read = read + '.';
+  this.info = function() {
+      return this.title + this.author + this.pages + this.read;
+  };
 }
 
 function addBookToLibrary() {
