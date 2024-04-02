6ee7c8aa1b8f575503d6e809a62f72e257ecafbd
Jake-Silveira <jacob1silveira@gmail.com>
2024-04-02 17:08:15 -0500

    worked on book popup, added radio buttons

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const book1 = new Book('Divergent', 'Veronica Roth', 487, 'has been read');
-const book2 = new Book('Insurgent', 'Veronica Roth', 568, 'has been read');
-const book3 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book4 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book5 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book6 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book7 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book8 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book9 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book10 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book11 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book12 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book13 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
-const book14 = new Book('Alegiant', 'Veronica Roth', 526, 'has not read yet');
+const book1 = new Book('Divergent', 'Veronica Roth', 487, 'read');
+const book2 = new Book('Insurgent', 'Veronica Roth', 568, 'unread');
+const book3 = new Book('Alegiant', 'Veronica Roth', 526, 'in-progress');
+const book4 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book5 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book6 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book7 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book8 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book9 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book10 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book11 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book12 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book13 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
+const book14 = new Book('Alegiant', 'Veronica Roth', 526, 'unread');
 
 const myLibrary = [book1, book2, book3, book4, book5, book6, book7, book8, book9, book10, book11, book12, book13, book14];
 
@@ -23,22 +23,10 @@ const overlay = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".btn-close");
 const bookTitle = document.querySelector(".bookTitle");
 const bookAuthor = document.querySelector(".bookAuthor");
-
-const openModal = function (title, author) {
-  bookTitle.textContent = title;
-  bookAuthor.textContent = author;
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-closeModalBtn.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-document.addEventListener("keydown", closeModal);
+const bookPages = document.querySelector(".bookPages");
+const bookRead = document.getElementById('read');
+const bookUnread = document.getElementById('unread');
+const bookInProgress =document.getElementById('in-progress');
 
 function makeRows(rows, cols){
   content.style.setProperty('--grid-rows', rows);
@@ -84,16 +72,59 @@ function displayBooks(){
       let cell = document.getElementById('grid-item' + i);
       let icon = document.createElement('img');
       icon.className = 'grid-item-icon';
+      icon.id = 'grid-item-icon' + i;
       icon.src = 'photos/book.svg';
       cell.style.opacity = '100';
       cell.textContent = String(myLibrary[i].title).substring(0, 10);
       cell.appendChild(icon);
       cell.addEventListener("click", () => {
-        openModal(myLibrary[i].title, myLibrary[i].author);
+        openModal(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].read);
       });
   };
 };
 };
+
+const openModal = function (title, author, pages, read) {
+  bookTitle.textContent = title;
+  bookAuthor.textContent = author;
+  bookPages.textContent = pages;
+  if(read == 'read'){
+    bookRead.checked = true;
+    bookUnread.checked = false;
+    bookInProgress.checked = false;
+  } else if(read == 'unread'){
+    bookRead.checked = false;
+    bookUnread.checked = true;
+    bookInProgress.checked = false;
+  } else if(read == 'in-progress'){
+    bookRead.checked = false;
+    bookUnread.checked = false;
+    bookInProgress.checked =true;
+  }
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+bookRead.addEventListener("click", () => {
+  bookUnread.checked = false;
+  bookInProgress.checked = false;
+});
+bookUnread.addEventListener("click", () => {
+  bookRead.checked = false;
+  bookInProgress.checked = false;
+});
+bookInProgress.addEventListener("click", () =>{
+  bookRead.checked = false;
+  bookUnread.checked = false;
+});
+
 
 
 /*Book.prototype.sayHello = function() {
